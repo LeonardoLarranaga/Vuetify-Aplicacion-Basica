6eecf9603c2eb033c1828dd16c1c51ea8f4ccd20
Leonardo <luis.larranaga@uabc.edu.mx>
2024-02-25 19:11:00 -0800

    Git init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+   <v-card>
+    <v-layout fluid style="height: 100vh;">
+      <v-navigation-drawer expand-on-hover rail>
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item
+            prepend-icon="mdi-tooltip-image"
+            title="My Files"
+            value="myfiles"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-account-multiple"
+            title="Shared with me"
+            value="shared"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-star"
+            title="Starred"
+            value="starred"
+          ></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+      <router-view />
+    </v-main>
+    </v-layout>
+  </v-card>
+
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
