bb1d6bb88c6fa5f679ea27c7879742ee1cd66b33
Leonardo <luis.larranaga@uabc.edu.mx>
2024-02-25 22:31:43 -0800

    Página de Imagen & Chiste

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
     <v-layout fluid style="height: 100vh;">
       <v-app-bar :elevation="2.5" rounded>
-        <v-app-bar-title>Aplicación Básica: Imágenes, Chistes & To-dos</v-app-bar-title>
+        <v-app-bar-title class="font-weight-bold">Aplicación Básica: Imágenes, Chistes & To-dos</v-app-bar-title>
       </v-app-bar>
 
-      <v-navigation-drawer expand-on-hover rail :width="275">
+      <v-navigation-drawer expand-on-hover rail :width=220>
         <v-list>
           <v-list-item 
             prepend-icon="mdi-home"
@@ -14,7 +14,7 @@
           ></v-list-item>
         </v-list>
 
-        <v-divider></v-divider>
+        <v-divider/>
 
         <v-list density="compact" nav>
           <v-list-item 
@@ -31,7 +31,9 @@
         </v-list>
       </v-navigation-drawer>
 
-      <v-main><router-view/></v-main>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
 
     </v-layout>
 </template>
---
+++ src/components/Imagen.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="450"
+  >
+
+  <template v-slot:loader="{ isActive }">
+      <v-progress-linear
+        :active="isActive"
+        color="indigo-darken-4"
+        height="4"
+        indeterminate/>
+    </template>
+  
+    <v-img
+      cover
+      height="250"
+      :src="this.imagen"
+      @load="this.loading = false"/>
+
+    <v-card-item>
+      <p class="text-indigo-darken-2"> {{ this.chiste.value }} </p>
+      <!--<p class="text-right text-medium-emphasis"> {{ this.chiste.updated_at }}</p>-->
+
+    </v-card-item>
+    
+    <v-divider class="mx-4 mb-1"/>
+
+    <v-card-actions>
+      <v-btn
+        color="indigo-darken-4"
+        variant="tonal"
+        @click="recargar"
+        block rounded="XL"
+        prepend-icon="mdi-dice-3"
+      >
+        Recargar
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      loading: false,
+      imagen: "",
+      chiste: {},
+    }),
+
+    mounted() {
+      this.recargar()
+    },
+
+    methods: {
+      recargar() {
+        this.loading = true
+        this.imagen = `https://picsum.photos/450/250?random=${Math.random()}`
+        this.fetchChiste()
+      },
+
+      async fetchChiste() {
+        try {
+          const response = await fetch("https://api.chucknorris.io/jokes/random")
+
+          if (response.ok) {
+            this.chiste = await response.json()
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      },
+    },
+  }
+</script>
